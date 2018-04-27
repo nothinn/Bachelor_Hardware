be1f3a51d82985d3128a5c256f65b9c05fe4d237
Anthon Vincent Riber <s154663@win.dtu.dk>
2018-04-27 13:32:14 +0200

    Splittet maxpool to seperate components

--- mnist_hardware/src/maxpool.vhd
+++ mnist_hardware/src/maxpool.vhd
@@ -7,47 +7,44 @@ entity maxpool is
 	port(
 		clk        : in  std_logic;
 		rst        : in  std_logic;
-		convResult : in  ram_input(NrOfINputs - 1 downto 0);
+		convResult : in  MAC_result;
 		newMax     : in  std_logic;
 		calcMax    : in  std_logic;
-		MaxResult  : out ram_input(NrOfInputs - 1 downto 0)
+		MaxResult  : out MAC_result
 	);
 end entity maxpool;
 
 architecture RTL of maxpool is
 
-	signal maxReg, maxRegNext, newMaxMux : ram_input(NrOfInputs - 1 downto 0);
+	signal maxReg, maxRegNext, newMaxMux : MAC_result;
 
 begin
 	Logic : process(all)
 	begin
-		for I in 0 to (NrOfInputs - 1) loop
-			case newMax is
-				when '1' =>
-					newMaxMux(I) <= (others => '0');
-				when others =>
-					newMaxMux(I) <= maxReg(I);
-			end case;
+		case newMax is
+			when '1' =>
+				newMaxMux <= (others => '0');
+			when others =>
+				newMaxMux <= maxReg;
+		end case;
 
-			maxRegNext(I) <= MAXIMUM(newMaxMux(I), convResult(I));
+		maxRegNext <= MAXIMUM(newMaxMux, convResult);
 
-			MaxResult(I) <= maxReg(I);
-		end loop;
+		MaxResult <= maxReg;
 	end process Logic;
 
 	RegisterTransfer : process(all)
 	begin
-		for I in 0 to (NrOfInputs - 1) loop
-			if rst = '1' then
-				maxReg(I) <= (others => '0');
-			elsif rising_edge(clk) then
-				if calcMax = '1' then
-					maxReg(I) <= maxRegNext(I);
-				else
-					null;
-				end if;
+		if rst = '1' then
+			maxReg <= (others => '0');
+		elsif rising_edge(clk) then
+			if calcMax = '1' then
+				maxReg <= maxRegNext;
+			else
+				null;
 			end if;
-		end loop;
+		end if;
+
 	end process RegisterTransfer;
 
 end architecture RTL;
