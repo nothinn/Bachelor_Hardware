c90ebb6d3cd10dfb41a52fec1a153704e18e606c
Anthon Vincent Riber <s154663@win.dtu.dk>
2018-05-30 14:47:29 +0200

    ready for multiple layers bias

--- mnist_hardware/src/biasRom.vhd
+++ mnist_hardware/src/biasRom.vhd
@@ -1,94 +1,93 @@
 library IEEE;
-    use IEEE.std_logic_1164.all;
-    use IEEE.numeric_std.all;
-    use work.Types.all;
-    use work.ConfigVHDL.all;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use work.Types.all;
+use work.ConfigVHDL.all;
 
 entity biasRom is
-    port (
-        clk: in  std_logic;
-        rst: in  std_logic;
-		filter: in integer range 0 to 31;
-		layer: in integer range 0 to nrOfLayers;
-        output: out signed(7 downto 0)
-    );
+	port(
+		clk    : in  std_logic;
+		rst    : in  std_logic;
+		filter : in  integer range 0 to 31;
+		layer  : in  integer range 0 to nrOfLayers;
+		output : out signed(7 downto 0)
+	);
 end entity;
 
 architecture rtl of biasRom is
-    
+
 begin
-    
-    
-    process(all)
-    begin
-        if rising_edge(clk) then
-            
-            case filter is
+
+	process(all)
+	begin
+		if rising_edge(clk) then
+
+			case filter is
 				when 0 =>
-						output <= "00000100";
+					output <= "00000100";
 				when 1 =>
-						output <= "00000000";
+					output <= "00000000";
 				when 2 =>
-						output <= "00000100";
+					output <= "00000100";
 				when 3 =>
-						output <= "11111000";
+					output <= "11111000";
 				when 4 =>
-						output <= "11110101";
+					output <= "11110101";
 				when 5 =>
-						output <= "00000011";
+					output <= "00000011";
 				when 6 =>
-						output <= "11111111";
+					output <= "11111111";
 				when 7 =>
-						output <= "00000111";
+					output <= "00000111";
 				when 8 =>
-						output <= "00001111";
+					output <= "00001111";
 				when 9 =>
-						output <= "00000000";
+					output <= "00000000";
 				when 10 =>
-						output <= "11111111";
+					output <= "11111111";
 				when 11 =>
-						output <= "11111100";
+					output <= "11111100";
 				when 12 =>
-						output <= "00000010";
+					output <= "00000010";
 				when 13 =>
-						output <= "00000001";
+					output <= "00000001";
 				when 14 =>
-						output <= "11111110";
+					output <= "11111110";
 				when 15 =>
-						output <= "11111000";
+					output <= "11111000";
 				when 16 =>
-						output <= "00000000";
+					output <= "00000000";
 				when 17 =>
-						output <= "00000011";
+					output <= "00000011";
 				when 18 =>
-						output <= "00000000";
+					output <= "00000000";
 				when 19 =>
-						output <= "00000100";
+					output <= "00000100";
 				when 20 =>
-						output <= "00000000";
+					output <= "00000000";
 				when 21 =>
-						output <= "11111101";
+					output <= "11111101";
 				when 22 =>
-						output <= "00000001";
+					output <= "00000001";
 				when 23 =>
-						output <= "11111100";
+					output <= "11111100";
 				when 24 =>
-						output <= "00000010";
+					output <= "00000010";
 				when 25 =>
-						output <= "00001010";
+					output <= "00001010";
 				when 26 =>
-						output <= "00000111";
+					output <= "00000111";
 				when 27 =>
-						output <= "00000010";
+					output <= "00000010";
 				when 28 =>
-						output <= "00000110";
+					output <= "00000110";
 				when 29 =>
-						output <= "11111101";
+					output <= "11111101";
 				when 30 =>
-						output <= "11111000";
+					output <= "11111000";
 				when others =>
-						output <= "11111110";
-            end case;
-        end if;
-    end process;
+					output <= "11111110";
+			end case;
+		end if;
+	end process;
 end architecture;
